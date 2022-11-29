5d6ced6407db57074bd71e5cd058d338daf2f768
tekfly <68149599+tekfly@users.noreply.github.com>
2022-11-29 10:49:29 +0000

    Update cert_generator_v2.ps1

--- cert_generator_v2.ps1
+++ cert_generator_v2.ps1
@@ -1,4 +1,4 @@
-#RUN AS ADMIN
+#RUN AS ADMIN##
 If (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]'Administrator')) {
 	Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
 	Exit
